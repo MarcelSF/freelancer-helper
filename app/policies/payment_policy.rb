01c7b1322b57c6ecd62adb7c69f8862401d54f53
Marcel Fonseca <marcelsf23br@gmail.com>
2020-06-15 08:51:35 -0300

    initial index implementation

--- app/policies/payment_policy.rb
+++ app/policies/payment_policy.rb
@@ -1,7 +1,7 @@
 class PaymentPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      scope.where(Payment.joins(project: {client: :user}).where(users: {id: user.id}).uniq)
     end
   end
 
