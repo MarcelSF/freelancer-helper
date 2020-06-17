60365f4ee4615aec27a4fe2192ebb325735033b9
Marcel Fonseca <marcelsf23br@gmail.com>
2020-06-17 17:06:36 -0300

    initial controller

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -4,10 +4,22 @@ class ClientsController < ApplicationController
   end
 
   def new
-
+    @client = Client.new
+    authorize @client
   end
 
   def create
+    @client = Client.new(client_params)
+    if @client.save
+      redirect to new_payment_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def client_params
 
   end
 end
